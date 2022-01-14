f2ff74ba025df4baa79a41eb8f7f2563137de0ab
tolo-inf <todordusaodcoveka00@gmail.com>
2022-01-14 15:31:57 +0100

    Uradjen projekat

---
+++ ProjekatWEB/SQL naredbe.sql
@@ -0,0 +1,211 @@
+INSERT INTO Igra (ID, Naziv, Zanr, GodinaIzlaska, Developer, Publisher)
+VALUES (1 , 'The Witcher 3: Wild Hunt' , 'RPG' , 2015 , 'CD Projekt Red' , 'CD Projekt Red');
+INSERT INTO Igra (ID, Naziv, Zanr, GodinaIzlaska, Developer, Publisher)
+VALUES (2 , 'God of War' , 'Action' , 2018 , 'Santa Monica' , 'Sony Interactive');
+INSERT INTO Igra (ID, Naziv, Zanr, GodinaIzlaska, Developer, Publisher)
+VALUES (3 , 'The Last of Us' , 'Survival' , 2013 , 'Naughty Dog' , 'Sony Interactive');
+INSERT INTO Igra (ID, Naziv, Zanr, GodinaIzlaska, Developer, Publisher)
+VALUES (4 , 'Halo Infinite' , 'FPS' , 2021 , '343 Industries' , 'XBOX');
+INSERT INTO Igra (ID, Naziv, Zanr, GodinaIzlaska, Developer, Publisher)
+VALUES (5 , 'DOOM' , 'FPS' , 2016 , 'id Software' , 'Bethesda');
+INSERT INTO Igra (ID, Naziv, Zanr, GodinaIzlaska, Developer, Publisher)
+VALUES (6 , 'Shadow of the Tomb Raider' , 'Action' , 2018 , 'Eidos Montreal' , 'Square Enix');
+INSERT INTO Igra (ID, Naziv, Zanr, GodinaIzlaska, Developer, Publisher)
+VALUES (7 , 'Resident Evil: Village' , 'Horror' , 2021 , 'Capcom' , 'Capcom');
+INSERT INTO Igra (ID, Naziv, Zanr, GodinaIzlaska, Developer, Publisher)
+VALUES (8 , 'Forza Horizon 5' , 'Racing' , 2021 , 'Turn 10 Studios' , 'XBOX');
+INSERT INTO Igra (ID, Naziv, Zanr, GodinaIzlaska, Developer, Publisher)
+VALUES (9 , 'Dark Souls 3' , 'RPG' , 2017 , 'From Software' , 'Bandai Namco');
+INSERT INTO Igra (ID, Naziv, Zanr, GodinaIzlaska, Developer, Publisher)
+VALUES (10 , 'The Legend of Zelda: Breath of the Wild' , 'RPG' , 2017 , 'Nintendo' , 'Nintendo');
+
+
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (1 , 5 , 5 , 5 , 5 , 1, 1);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (2 , 5 , 5 , 5 , 4 , 1, 2);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (3 , 4 , 5 , 5 , 5 , 1, 3);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (4 , 5 , 5 , 5 , 5 , 1, 4);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (5 , 5 , 5 , 5 , 5 , 1, 5);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (6 , 5 , 5 , 5 , 5 , 2, 1);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (7 , 4 , 4 , 5 , 5 , 2, 2);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (8 , 5 , 3 , 5 , 5 , 2, 3);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (9 , 5 , 5 , 5 , 5 , 2, 4);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (10 , 5 , 4 , 5 , 5 , 2, 5);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (11 , 4 , 4 , 4 , 4 , 3, 1);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (12 , 5 , 3 , 3 , 5 , 3, 2);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (13 , 5 , 5 , 5 , 4 , 3, 3);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (14 , 5 , 5 , 5 , 5 , 3, 4);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (15 , 4 , 5 , 5 , 5 , 3, 5);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (16 , 3 , 2 , 3 , 4 , 4, 1);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (17 , 4 , 2 , 3 , 3 , 4, 2);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (18 , 3 , 1 , 2 , 4 , 4, 3);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (19 , 4 , 4 , 5 , 5 , 4, 4);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (20 , 5 , 3 , 5 , 5 , 4, 5);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (21 , 5 , 3 , 5 , 4 , 5, 1);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (22 , 4 , 4 , 4 , 4 , 5, 2);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (23 , 3 , 4 , 5 , 5 , 5, 3);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (24 , 5 , 4 , 4 , 5 , 5, 4);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (25 , 5 , 5 , 5 , 5 , 5, 5);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (26 , 3 , 3 , 3 , 5 , 6, 1);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (27 , 4 , 4 , 3 , 4 , 6, 2);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (28 , 4 , 5 , 2 , 5 , 6, 3);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (29 , 5 , 5 , 4 , 4 , 6, 4);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (30 , 3 , 2 , 5 , 5 , 6, 5);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (31 , 5 , 4 , 5 , 5 , 7, 1);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (32 , 5 , 4 , 5 , 4 , 7, 2);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (33 , 3 , 4 , 5 , 5 , 7, 3);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (34 , 3 , 4 , 4 , 5 , 7, 4);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (35 , 4 , 4 , 4 , 3 , 7, 5);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (36 , 5 , 2 , 5 , 5 , 8, 1);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (37 , 4 , 3 , 5 , 5 , 8, 2);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (38 , 5 , 5 , 5 , 5 , 8, 3);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (39 , 5 , 4 , 4 , 5 , 8, 4);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (40 , 5 , 3 , 5 , 5 , 8, 5);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (41 , 1 , 2 , 4 , 3 , 9, 1);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (42 , 4 , 5 , 5 , 3 , 9, 2);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (43 , 4 , 3 , 3 , 3 , 9, 3);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (44 , 4 , 4 , 5 , 5 , 9, 4);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (45 , 5 , 5 , 5 , 5 , 9, 5);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (46 , 5 , 4 , 4 , 4 , 10, 1);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (47 , 5 , 5 , 5 , 5 , 10, 2);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (48 , 4 , 3 , 5 , 5 , 10, 3);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (49 , 4 , 5 , 5 , 4 , 10, 4);
+INSERT INTO Ocena (ID, Gameplay, Story, Music, Graphics, IgraFKID, KorisnikFKID)
+VALUES (50 , 5 , 5 , 4 , 3 , 10, 5);
+
+
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (1, 'GameAwards' , 'GOTY' , 1);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (2, 'GameAwards' , 'Best RPG' , 1);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (3, 'BAFTA' , 'GOTY' , 1);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (4, 'Golden Joystick' , 'Golden Game' , 1);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (5, 'Critics Choice' , 'Best Choice' , 1);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (6, 'GameAwards' , 'GOTY' , 2);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (7, 'GameAwards' , 'Best Music' , 2);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (8, 'BAFTA' , 'Narative Masterpiece' , 2);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (9, 'Golden Joystick' , 'Golden Gameplay' , 2);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (10, 'Critics Choice' , 'Chosen One' , 3);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (11, 'GameAwards' , 'GOTY' , 3);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (12, 'BAFTA' , 'Best Action' , 3);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (13, 'Golden Joystick' , 'Golden Gun' , 4);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (14, 'GameAwards' , 'Best FPS' , 5);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (15, 'Critics Choice' , 'Shooter Legend' , 5);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (16, 'GameAwards' , 'Best Narative' , 7);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (17, 'BAFTA' , 'Narative Masterpiece' , 7);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (18, 'Golden Joystick' , 'Golden Game' , 7);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (19, 'Critics Choice' , 'Best Racing' , 8);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (20, 'Golden Joystick' , 'Golden Wheel' , 8);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (21, 'GameAwards' , 'Most Anticipated' , 9);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (22, 'BAFTA' , 'Gameplay Masterpiece' , 9);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (23, 'Golden Joystick' , 'Golden Sword' , 9);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (24, 'GameAwards' , 'GOTY' , 10);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (25, 'GameAwards' , 'Best Open-World' , 10);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (26, 'Golden Joystick' , 'Golden Game' , 10);
+INSERT INTO Nagrada (ID, NazivOrg, Kategorija, IgraFKID)
+VALUES (27, 'Critics Choice' , 'Chosen One' , 10);
+
+INSERT INTO Prodavnica (ID, CenaIgre, KolicinaProdatih, IgraFKID)
+VALUES (1, 60 , 7000000, 1);
+INSERT INTO Prodavnica (ID, CenaIgre, KolicinaProdatih, IgraFKID)
+VALUES (2, 70 , 5000000, 2);
+INSERT INTO Prodavnica (ID, CenaIgre, KolicinaProdatih, IgraFKID)
+VALUES (3, 50 , 2500000, 3);
+INSERT INTO Prodavnica (ID, CenaIgre, KolicinaProdatih, IgraFKID)
+VALUES (4, 80 , 500000, 4);
+INSERT INTO Prodavnica (ID, CenaIgre, KolicinaProdatih, IgraFKID)
+VALUES (5, 30 , 1200000, 5);
+INSERT INTO Prodavnica (ID, CenaIgre, KolicinaProdatih, IgraFKID)
+VALUES (6, 60 , 930000, 6);
+INSERT INTO Prodavnica (ID, CenaIgre, KolicinaProdatih, IgraFKID)
+VALUES (7, 80 , 1250000, 7);
+INSERT INTO Prodavnica (ID, CenaIgre, KolicinaProdatih, IgraFKID)
+VALUES (8, 90 , 1550000, 8);
+INSERT INTO Prodavnica (ID, CenaIgre, KolicinaProdatih, IgraFKID)
+VALUES (9, 100 , 3100000, 9);
+INSERT INTO Prodavnica (ID, CenaIgre, KolicinaProdatih, IgraFKID)
+VALUES (10, 70 , 2150000, 10);
+
+
+INSERT INTO Korisnik (ID, Username)
+VALUES (1, 'VuciLegenda00');
+INSERT INTO Korisnik (ID, Username)
+VALUES (2, 'tol3madjija');
+INSERT INTO Korisnik (ID, Username)
+VALUES (3, 'PewDiePie');
+INSERT INTO Korisnik (ID, Username)
+VALUES (4, 'JefkaCarica');
+INSERT INTO Korisnik (ID, Username)
+VALUES (5, 'GameDestroyer99');
